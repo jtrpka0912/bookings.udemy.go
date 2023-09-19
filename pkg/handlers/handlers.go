4a6d6d8dd5874ad7881a0ef58309b3663088dfe7
J. Trpka <jtrpka0912@gmail.com>
2023-09-18 20:25:52 -0400

    Partial room completion.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -47,3 +47,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+// Temporary
+
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
