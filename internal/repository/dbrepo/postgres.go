8b115ebade7c8617982ce261b28d2a77eaca7ad9
Jeremy Trpka <jtrpka@signs365.com>
2023-10-31 08:09:37 -0400

    Tested insert reservation and fixed up query.

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -16,11 +16,9 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 
-	stmt := `
-	INSERT INTO reservations 
+	stmt := `INSERT INTO reservations 
 		(first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at)
-	VALUES ($1, $2, $3, $4, $5, %6, %7, $8, $9)
-	`
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id`
 
 	_, err := m.DB.ExecContext(ctx, stmt,
 		res.FirstName,
@@ -31,7 +29,8 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 		res.EndDate,
 		res.RoomID,
 		time.Now(),
-		time.Now())
+		time.Now(),
+	)
 
 	if err != nil {
 		return err
